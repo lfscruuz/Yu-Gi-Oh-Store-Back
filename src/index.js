298cdea359858d1ea63731d7e5cf397466e19a9d
luis <lfscruz2001@gmail.com>
2022-11-23 00:32:48 -0300

    cors adicionado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening in port ${port}`))
