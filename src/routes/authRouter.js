c8f744b858dcb2abf1f93565024162d23ced842d
Thais Reis <reis.thaisf@gmail.com>
2022-11-25 11:42:00 -0300

    Update authRouter.js

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,12 +3,10 @@ import { Router } from 'express';
 // Import the singup function from authControllers.js
 import { singup } from '../controllers/authControllers.js';import { signup, postLogin} from '../controllers/authControllers.js';
 import {sessionMiddleware, loginMiddleware} from '../middlewares/userMiddlewares.js';
-import userMiddlewares from '../middlewares/userMiddlewares.js';
-
 
 const authRouter = Router();
 
-authRouter.post('/singup', singup, {sessionMiddleware);
+authRouter.post('/singup', singup, sessionMiddleware);
 authRouter.post("/login", loginMiddleware, postLogin);
 
 export default authRouter;
