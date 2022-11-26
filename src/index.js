d140a39b64919c7311fe66d2fba980333737777f
luis <lfscruz2001@gmail.com>
2022-11-26 15:47:04 -0300

    porta corrigida

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/products', productsRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
