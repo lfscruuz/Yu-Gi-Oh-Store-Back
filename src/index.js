9f7336f5733188b6d08f381397c0f1fb19896860
luis <lfscruz2001@gmail.com>
2022-11-27 16:02:51 -0300

    rota corrigida

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(authRouter);
 app.use(productsRouter);
 
-// const port = process.env.PORT || 5000;
-const port = 5000
+const port = process.env.PORT || 5000;
+// const port = 5000
 
 app.listen(port, () => {
     console.log("Server running on port " + port);
